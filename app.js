7f3d3a8777ea6585efb0b3597fa40e622c831571
Samuli Virtanen <samuliedvin@gmail.com>
2018-05-22 18:46:20 +0300

    Viimeistelyä

--- app.js
+++ app.js
@@ -1,29 +1,19 @@
-var music, 
-    s, 
-    scalemanager, 
-    text, 
-    otsikko,
-    waveform;
-
 var slices, 
     sprites, 
-    stars;
+    stars, // tarvittavat taulukot globaalina
+    sine;
 
-var sine;
+var time_til_spawn,
+    last_spawn_time; // aikamuuttujat tähdenlentoja varten
 
-var time_til_spawn;
-var last_spawn_time;
+/*
+ * Luodaan tämän demon peli-instanssi
+ */ 
 
-var xl;
-var cx = 0;
-
-
-
-// Luodaan tämän demon peli-instanssi
 var game = new Phaser.Game(800,
     600,
     Phaser.WEBGL,
-    'body',
+    'phaser',
     {
         preload: preload,
         create: create,
@@ -31,10 +21,8 @@ var game = new Phaser.Game(800,
     });
 
 
-// Ladataan musat ja taustakuva muistiin
+// Ladataan musat, tähti ja taustakuva muistiin
 function preload() {
-    game.load.audio('slowmo', ['assets/slowmo.mp3', 'assets/slowmo.ogg']);
-    game.load.image('background', 'assets/background.png'); 
     
     // Tähti 
     var starData = [
@@ -42,12 +30,9 @@ function preload() {
         '222',
         '.2.'
     ];
-    
-
-    // game.create.texture('star', starData, 3, 3, 0); // luodaan taulukkodatan pohjalta tähden tekstuuri
+    game.load.audio('slowmo', ['assets/slowmo.mp3', 'assets/slowmo.ogg']);
+    game.load.image('background', 'assets/background.png'); 
     game.load.imageFromTexture('star', starData, 3, 3, 0);
-    
-
 }
 
 /* Luodaan kaikki tarvittava:
@@ -66,15 +51,12 @@ function create() {
     bg = game.add.tileSprite(0, 0, 800, 600, 'background');
 
     // Musat päälle
-	music = game.add.audio('slowmo');
+	var music = game.add.audio('slowmo');
 	music.loop = true;
     music.play();
-    
-    // Retro frameratet tulille -- Ei toiminut toivotusti?
-    game.time.suggestedFps = 10;
         
     // Lisätään otsikko
-    otsikko = game.add.text(290, 30, "Phaser.io", {font: 'italic 20pt Times', fontSize: 64, fill: '#ffffff' });
+    var otsikko = game.add.text(290, 30, "Phaser.io", {font: 'italic 20pt Times', fontSize: 64, fill: '#ffffff' });
     
     // Laitetaan otsikko välkkymään
     otsikko.alpha = 0;
@@ -118,14 +100,14 @@ function create() {
     }
     
     
-    starlet = game.add.sprite(-5, -5, 'star'); 
+    extraStar = game.add.sprite(-10, -10, 'star'); 
     // nähtävästi peliin on luotava sprite että sen pohjalta voi luoda uusia silmukassa, 
-    // ei nyt keksitty muutakaan niin laitetaan piiloon (-5, -5)
+    // ei nyt keksitty muutakaan niin laitetaan piiloon (-10, -10)
     
     
    
 
-    var starAmount = 15; // Tähtien määrä 
+    var starAmount = 25; // Tähtien määrä 
     
     
     // Tähtitaivaan luonti (randomilla)
@@ -138,7 +120,7 @@ function create() {
 
             // Laitetaan tähti tuikkimaan eli vaihtelee läpinäkyvyyttä 0.3-1 välillä. Käytetään phaserin tween-luokkaa.
             star.alpha = 0.3;
-            game.add.tween(star).to( { alpha: 1}, 2000, Phaser.Easing.Linear.None, true, x*10, 1000, true);
+            game.add.tween(star).to( { alpha: 1}, 2000, Phaser.Easing.Linear.None, true, game.rnd.between(0,3000), -1, true);
 
             sprites.addChild(star);
             stars.push(star);
@@ -153,9 +135,10 @@ function create() {
     
 
     // Tähdenlentojen randomi esiintyminen
-    time_til_spawn = Math.random()*3000 + 2000;  //Random time between 2 and 5 seconds.
+    time_til_spawn = Math.random()*3000 + 2000;  //randomi aika 2-5 sek
     last_spawn_time = game.time.time;   
-}
+    
+} // create() 
 
 /*
 
@@ -193,19 +176,18 @@ function update() {
 
 // Tähdenlentojen luonti
 function spawnShootingStar() {
-    var x = game.rnd.between(0, 800);
+    var x = game.rnd.between(0, 800); // Lähtöpiste
     var y = game.rnd.between(0, 400);
 
-    var x2 = game.rnd.between(0, 800);
+    var x2 = game.rnd.between(0, 800); // Päätepiste
     var y2 = game.rnd.between(0, 400);
     
     
     var shootingStar = game.make.sprite(x, y, 'star');
 
     shootingStar.alpha = 0;
-    game.add.tween(shootingStar).to( { x: x2, y: y2 }, 2000, Phaser.Easing.Linear.None, true, 0, 0, false);
-    game.add.tween(shootingStar).to( { alpha: 1 }, 1000, Phaser.Easing.Linear.None, true, 0, 0, true);
+    game.add.tween(shootingStar).to( { x: x2, y: y2 }, 2000, Phaser.Easing.Linear.None, true, 0, 0, false); // Liikkuu alkupisteestä päätepisteeseen
+    game.add.tween(shootingStar).to( { alpha: 1 }, 1000, Phaser.Easing.Linear.None, true, 0, 0, true); // Fade in & out
 
-    sprites.addChild(shootingStar);
-    stars.push(shootingStar);
+    sprites.addChild(shootingStar); // lisäillään spritebatchiin
 }
